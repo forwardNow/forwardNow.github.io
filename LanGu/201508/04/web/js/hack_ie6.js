fc59ba7d20dc58abd8b3502d58da806be5c8e577
wuqinfei <wuqinfei_cs@163.com>
2015-08-17 20:13:18 +0800

    蔡甸公安-去掉png背景图

--- LanGu/201508/04/web/js/hack_ie6.js
+++ LanGu/201508/04/web/js/hack_ie6.js
@@ -1,9 +1,10 @@
 $( function () {
 
   // fix <img>
+  /*
   window.setTimeout( function() {
     
-    $( "img" ).attr( "style", function ( index, value ) {
+    $( "img[src$='.png']" ).attr( "style", function ( index, value ) {
       value = value || "";
       var filter = value + ';filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src="'+this.src+'")';
       this.src = "./images/transparent.gif";
@@ -11,7 +12,7 @@ $( function () {
     } );
 
   }, 3000 );
-
+  */
 
 
   $( ".news-date" ).each( function ( index, elt ) {
