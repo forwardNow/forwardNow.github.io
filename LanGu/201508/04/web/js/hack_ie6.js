cc4d640327c0440b2df324e508e43ae52dd19905
wuqinfei <wuqinfei_cs@163.com>
2015-08-17 18:13:08 +0800

    修改蔡甸公安网站-ie6-图片载入后执行

--- LanGu/201508/04/web/js/hack_ie6.js
+++ LanGu/201508/04/web/js/hack_ie6.js
@@ -1,12 +1,14 @@
 $( function () {
 
   // fix <img>
-  $( "img" ).attr( "style", function ( index, value ) {
+  $( "img" ).load(function () {
+    $( this ).attr( "style", function ( index, value ) {
     value = value || "";
     var filter = value + ';filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src="'+this.src+'")';
     this.src = "./images/transparent.gif";
     return filter;
-  } );
+  } )
+  });
 
   $( ".news-date" ).each( function ( index, elt ) {
     $(this).parent().prepend( this );
