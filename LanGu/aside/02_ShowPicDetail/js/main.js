0885412852327eb6a75b806473b3e2ddfece803c
wuqinfei <wuqinfei_cs@163.com>
2015-09-23 22:48:48 +0800

    图片放大

--- LanGu/aside/02_ShowPicDetail/js/main.js
+++ LanGu/aside/02_ShowPicDetail/js/main.js
@@ -2,50 +2,124 @@
  * Created by forwardNow on 9/21/15.
  */
 
-$( function () {
+var preview = {
+    options: {
+        self: "#preview",
+        zoomMain: ".zoom_main",
+        zoomMainImg: ".img",
+        zoomMainCursor: ".cursor",
+        zoomDetail: ".zoom_detail",
+        zoomDetailImg: ".img"
+    },
+    init: function () {
+        this.render();
+        this.bind();
+        this.thumbnail.init();
+    },
+    render: function () {
+        this.self = $( this.options.self );
+        this.zoomMain = $( this.options.zoomMain, this.self );
+        this.zoomMainImg = $( this.options.zoomMainImg, this.zoomMain );
+        this.zoomMainCursor = $( this.options.zoomMainCursor, this.zoomMain );
+        this.zoomDetail = $( this.options.zoomDetail, this.self );
+        this.zoomDetailImg = $( this.options.zoomDetailImg, this.zoomDetail );
+    },
+    bind: function () {
+        var self = this;
+        var zoomMain = self.zoomMain;
+        var zoomMainCursor = self.zoomMainCursor;
+        var zoomMainImg = self.zoomMainImg;
+        var zoomDetailImg = self.zoomDetailImg;
+        var zoomMainDocPos = self.zoomMain.offset();
+        zoomMain.mousemove( function ( e ) {
+            var cursorWidth = zoomMainCursor.width(),
+                cursorHeight = zoomMainCursor.height(),
+                mouseOffsetPos = {
+                    x: e.pageX - zoomMainDocPos.left,
+                    y: e.pageY - zoomMainDocPos.top
+                },
+                cursorLeft,
+                cursorTop;
 
-    var preview = $( "#preview" ),
-        zoomMain = $( ".zoom_main" ),
-        zoomMainImg = zoomMain.find( ".img" ),
-        zoomMainCursor = zoomMain.find( ".cursor" ),
-        zoomMainDocPos = zoomMain.offset();
-    ;
+            moveCursor();
 
-    // 选中的区域
-    zoomMain.mousemove( function ( e ) {
+            viewDetail();
 
-        var cursorWidth = zoomMainCursor.width(),
-            cursorHeight = zoomMainCursor.height(),
-            mouseOffsetPos = {
-                x: e.pageX - zoomMainDocPos.left,
-                y: e.pageY - zoomMainDocPos.top
-            },
-            cursorLeft,
-            cursorTop;
+            function viewDetail() {
+                //console.info( zoomDetail, zoomDetailImg );
+                zoomDetailImg.css( {
+                    left: -cursorLeft / zoomMainImg.width() * zoomDetailImg.width(),
+                    top: -cursorTop / zoomMainImg.height() * zoomDetailImg.height()
+                } );
+            }
 
-        // 最大值
-        if ( mouseOffsetPos.x + cursorWidth / 2 > zoomMain.width() ) {
-            cursorLeft = zoomMain.width() - cursorWidth;
-        } else {
-            cursorLeft = mouseOffsetPos.x - cursorWidth / 2;
-        }
+            // 移动光标区域
+            function moveCursor() {
 
-        if ( mouseOffsetPos.y + cursorHeight / 2 > zoomMain.height() ) {
-            cursorTop = zoomMain.height() - cursorHeight;
-        } else {
-            cursorTop = mouseOffsetPos.y - cursorHeight / 2;
-        }
+                // 最大值
+                if ( mouseOffsetPos.x + cursorWidth / 2 > zoomMain.width() ) {
+                    cursorLeft = zoomMain.width() - cursorWidth;
+                } else {
+                    cursorLeft = mouseOffsetPos.x - cursorWidth / 2;
+                }
+                if ( mouseOffsetPos.y + cursorHeight / 2 > zoomMain.height() ) {
+                    cursorTop = zoomMain.height() - cursorHeight;
+                } else {
+                    cursorTop = mouseOffsetPos.y - cursorHeight / 2;
+                }
+                // 最小值
+                cursorLeft = cursorLeft > 0 ? cursorLeft : 0;
+                cursorTop = cursorTop > 0 ? cursorTop : 0;
+                //console.info( cursorLeft, cursorTop );
+                zoomMainCursor.css( {
+                    left: cursorLeft,
+                    top: cursorTop
+                } );
+            }
+        } ).mouseover( function () {
+            self.zoomDetail.show();
 
-        // 最小值
-        cursorLeft = cursorLeft > 0 ? cursorLeft : 0;
-        cursorTop = cursorTop > 0 ? cursorTop : 0;
-
-        zoomMainCursor.css( {
-            left: cursorLeft,
-            top: cursorTop,
+        } ).mouseout( function () {
+            self.zoomDetail.hide();
         } );
+    }
+};
 
-    } );
+
+preview.thumbnail = {
+    options: {
+        self: ".thumbnail",
+        list: ".list",
+        items: ".item",
+        img: ".img",
+        selecteditemClass: "item_selected"
+    },
+    init: function () {
+        this.render();
+        this.bind();
+    },
+    render: function () {
+        this.self = $( this.options.self );
+        this.list = $( this.options.list, this.self );
+        this.items = $( this.options.items, this.self );
+    },
+    bind: function () {
+        var self = this;
+        // 点击缩略图，更换图片
+        this.items.click( function () {
+
+            var img = $( this ).find( self.options.img ),
+                midImgSrc = img.attr( "data-mid-img" ),
+                bigImgSrc = img.attr( "data-big-img" );
+            preview.zoomMainImg.attr( "src", midImgSrc );
+            preview.zoomDetailImg.attr( "src", bigImgSrc );
+            self.items.removeClass( self.options.selecteditemClass );
+            $( this ).addClass( self.options.selecteditemClass );
+        } );
+    }
+};
+$( function () {
+    preview.init();
 
 
 } );
--- LanGu/java/Tools/src/io/github/forwardnow/pic/PictureUtils.java
+++ LanGu/java/Tools/src/io/github/forwardnow/pic/PictureUtils.java
@@ -3,6 +3,7 @@ package io.github.forwardnow.pic;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -25,7 +26,7 @@ public final class PictureUtils {
 	 * @param height
 	 *            转换后图片高度
 	 */
-	public static void resizeImage( String srcImgPath, String distImgPath, int width, int height ) throws IOException {
+	public static void resizeImage( String srcImgPath, String distImgPath, int width, int height, String imgType ) throws IOException {
 
 		File srcFile = new File( srcImgPath );
 		Image srcImg = ImageIO.read( srcFile );
@@ -33,8 +34,26 @@ public final class PictureUtils {
 		buffImg = new BufferedImage( width, height, BufferedImage.TYPE_INT_RGB );
 		buffImg.getGraphics( ).drawImage( srcImg.getScaledInstance( width, height, Image.SCALE_SMOOTH ), 0, 0, null );
 
-		ImageIO.write( buffImg, "JPEG", new File( distImgPath ) );
+		ImageIO.write( buffImg, imgType.toUpperCase( ), new File( distImgPath ) );
 
 	}
 
+	public static void main( String[] args ) throws Exception {
+		// File file = new File( "resource/images/2.800x800.png" );
+		// new FileInputStream( file );
+		for ( int i = 1; i <= 1; i++ ) {
+			String filename = i + "";
+			String srcImgPath = "resource/images/" + filename + ".800x800.png";
+			String distImgPath = "resource/images/" + filename + ".800x800.png";
+			int midWidth = 400;
+			int midHeight = 400;
+			int smallWidth = 50;
+			int smallHeight = 50;
+			String imgType = "PNG";
+			// mid
+			resizeImage( srcImgPath, distImgPath.replaceAll( "800", "400" ), midWidth, midHeight, imgType );
+			// small
+			resizeImage( srcImgPath, distImgPath.replaceAll( "800", "50" ), smallWidth, smallHeight, imgType );
+		}
+	}
 }
