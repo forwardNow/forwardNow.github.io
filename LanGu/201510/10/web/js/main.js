0b7934d7eda443fbd75f7d57fb3c32baf57ed89f
wuqinfei <wuqinfei_cs@163.com>
2015-10-14 18:13:04 +0800

    [武汉怡月佳家好月圆月子会所]完成index.html

--- LanGu/201510/10/web/js/main.js
+++ LanGu/201510/10/web/js/main.js
@@ -1,20 +1,194 @@
+// 在线报名 floor_1_reg
+function changeGender( elt ) {
+    // 0: 男；1: 女
+
+    var o = {
+            "0": "先生",
+            "1": "女士"
+        },
+        gender = $( elt ),
+        input = gender.find( "input[type='hidden']" ),
+        flag = "0";
+    if ( input.val() == "0" ) {
+        flag = "1";
+    } else {
+        flag = "0";
+    }
+
+    gender.find( ".text" ).text( o[ flag ] );
+    input.val( flag )
+}
+
 $( function () {
+    // 主页nav
     $( ".js__nav .item" ).bind( "mouseenter", onHander ).bind( "mouseleave", offHandler );
 
-    function onHander () {
-        var submenu = $( this ).find(".submenu");
-        $( this ).addClass("active");
+    function onHander() {
+        var submenu = $( this ).find( ".submenu" );
+        $( this ).addClass( "active" );
         if ( submenu.size() > 0 ) {
             $( this ).addClass( "has-submenu" );
             submenu.stop().slideDown();
         }
     }
-    function offHandler () {
-        var submenu = $( this ).find(".submenu");
-        $( this ).removeClass("active");
+
+    function offHandler() {
+        var submenu = $( this ).find( ".submenu" );
+        $( this ).removeClass( "active" );
         if ( submenu.size() > 0 ) {
             $( this ).removeClass( "has-submenu" );
             submenu.hide();
         }
     }
+
+
+    // 新闻模块 floor_1__news
+    $( ".js--floor_1__news .list .item" ).bind( "mouseenter", function () {
+        $( this ).addClass( "active" ).siblings().removeClass( "active" );
+    } ).bind( "mouseleave", function () {
+        $( this ).removeClass( "active" );
+        $( ".js--floor_1__news .list .item:eq(0)" ).addClass( "active" );
+    } );
 } );
+
+
+function Slide( options ) {
+    this.options = options;
+    this.init();
+}
+Slide.prototype.init = function () {
+    this.render();
+    this.bind();
+};
+
+Slide.prototype.render = function () {
+    this.type = this.options.type;
+    this.slideContainer = this.options.slideContainer;
+    this.pageList = this.options.pageList;
+    this.indexs = this.options.indexs;
+    this.prevBtn = this.options.prevBtn;
+    this.nextBtn = this.options.nextBtn;
+
+    this.pageSize = this.indexs.size();
+
+    this.pageIndex = 0; // 索引
+
+};
+
+Slide.prototype.bind = function () {
+    var self = this;
+
+    // index
+    this.indexs.bind( "mouseenter", indexHoverHandler );
+
+    // btn
+    this.prevBtn.bind( "click", clickPrevBtnHandler );
+    this.nextBtn.bind( "click", clickNextBtnHandler );
+
+    function indexHoverHandler() {
+        doSlide( $( this ).index() );
+    }
+
+    function clickPrevBtnHandler() {
+        doSlide( (self.pageIndex - 1 + self.pageSize) % self.pageSize );
+    }
+
+    function clickNextBtnHandler() {
+        doSlide( (self.pageIndex + 1) % self.pageSize );
+    }
+
+    function doSlide( index ) {
+        self.play( self.type, index );
+        self.pageIndex = index;
+    }
+};
+
+Slide.prototype.play = function ( type, index ) {
+    this.animate[ type ].call( this, index );
+    this.indexs.eq( index ).addClass( "active" ).siblings().removeClass( "active" );
+};
+
+Slide.prototype.animate = {
+    scroll: function ( index ) {
+        var distance = this.options.scroll.distance,
+            amount = this.options.scroll.amount || 1,
+            totalDistance, unitPos;
+
+        unitPos = distance.search( /[^0-9]/ );
+        totalDistance = -parseFloat( distance ) * amount * index + distance.substr( unitPos );
+        this.pageList.stop().animate( { left: totalDistance }, this.options.scroll.duration );
+
+    }
+};
+
+(function () {
+})();
+var options_banner = {
+    type: "scroll",
+    scroll: {
+        distance: "100%",
+        amount: 1,
+        duration: 300, // 滚动所需时间
+        delay: 3000    // 自动播放时的延迟
+    },
+    slideContainer: $( ".js--slideContainer_banner" ),
+    pageList: $( ".js--slideContainer_banner .page-list" ),
+    indexs: $( ".js--slideContainer_banner .tab .index" ),
+    prevBtn: $( ".js--slideContainer_banner .btn-prev" ),
+    nextBtn: $( ".js--slideContainer_banner .btn-next" )
+
+};
+var options_clubEnv = {
+    type: "scroll",
+    scroll: {
+        distance: "490px",
+        amount: 1,
+        duration: 300, // 滚动所需时间
+        delay: 3000    // 自动播放时的延迟
+    },
+    slideContainer: $( ".js--slideContainer_ClubEnv" ),
+    pageList: $( ".js--slideContainer_clubEnv .page-list" ),
+    indexs: $( ".js--slideContainer_clubEnv .tab .index" ),
+    prevBtn: $( ".js--slideContainer_clubEnv .btn-prev" ),
+    nextBtn: $( ".js--slideContainer_clubEnv .btn-next" )
+
+};
+var options_case = {
+    type: "scroll",
+    scroll: {
+        distance: "102%",
+        amount: 1,
+        duration: 300, // 滚动所需时间
+        delay: 3000    // 自动播放时的延迟
+    },
+    slideContainer: $( ".js--slideContainer_case" ),
+    pageList: $( ".js--slideContainer_case .page-list" ),
+    indexs: $( ".js--slideContainer_case .tab .index" ),
+    prevBtn: $( ".js--slideContainer_case .btn-prev" ),
+    nextBtn: $( ".js--slideContainer_case .btn-next" )
+
+};
+
+new Slide( options_banner );
+new Slide( options_case );
+
+var clubEnvSlide = new Slide( options_clubEnv ),
+    originPlay = clubEnvSlide.constructor.prototype.play;
+
+clubEnvSlide.play = function ( type, index ) {
+    originPlay.apply( this, arguments );
+    var pages = this.pageList.find( ".page" ),
+        index = index + 1;
+
+    pages.removeClass( "active active_prev active_next" );
+    pages.eq( index ).addClass( "active" );
+    pages.eq( index - 1 ).addClass( "active_prev" );
+    pages.eq( index + 1 ).addClass( "active_next" );
+};
+clubEnvSlide.nextBtn.click();
+
+
+
+
+
+
