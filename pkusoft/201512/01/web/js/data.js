eb22c1228bde0b0f053f5db15a98d3ed587a6eaf
wuqinfei <wuqinfei_cs@163.com>
2015-12-04 10:50:02 +0800

    [pkusoft]轮播和图片放大

--- pkusoft/201512/01/web/js/data.js
+++ pkusoft/201512/01/web/js/data.js
@@ -66,7 +66,7 @@ var data =
                             "quantity": 22,
                             "capacity": 100,
                             "icon": "warning"
-                        },
+                        }
                     ]
                 },
 
